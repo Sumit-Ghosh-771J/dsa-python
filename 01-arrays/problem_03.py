162eb0384589c7144e6d8eea7cb6bc524af91be6
Sumit Ghosh <sg9897602@gmail.com>
2026-08-21 01:43:23 +0530

    Delete 01-arrays directory

--- 01-arrays/problem_03.py
+++
@@ -1,7 +0,0 @@
-"""
-3. Multiplication Table
-"""
-
-a=int(input("Enter a no.: "))
-for i in range (1,11):
- print(f"{a}*{i}={a*i}")
--- 01-arrays/problem_12.py
+++
@@ -1,8 +0,0 @@
-"""
-11. Tuple
-"""
-
-x=tuple(input("Enter the colors: ").split())
-
-print(x)
-print(type(x))
