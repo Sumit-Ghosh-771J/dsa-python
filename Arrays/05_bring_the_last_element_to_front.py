d9da64a4a5785ace53ea2e0119fa4a55e8470561
Sumit Ghosh <sg9897602@gmail.com>
2026-08-21 02:14:08 +0530

    Delete Arrays directory

--- Arrays/05_bring_the_last_element_to_front.py
+++
@@ -1,8 +0,0 @@
-"""
-5. Bring the last element to front
-"""
-
-arr=list(map(int,input("Enter ur elements: ").split()))
-for i in range(len(arr)):
-    arr[i],arr[len(arr)-1]=arr[len(arr)-1],arr[i]
-print(f"{arr}")
--- Arrays/09_find_the_missing_number_from_the_range_of_an_array.py
+++
@@ -1,6 +0,0 @@
-"""
-9. Find the missing number from the range of an array
-"""
-
-arr=list(map(int,input("Enter your elements(starting from 0): ").split()))
-print(f"Missing no. from range {min(arr)}-{len(arr)} is : {(len(arr)*(len(arr)+1))//2-sum(arr)}")
