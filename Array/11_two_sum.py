087c15eceef1ee002ca891f41ea30c0d302026c1
Sumit-Ghosh-771J <Sumit-Ghosh-771J@users.noreply.github.com>
2026-08-21 20:52:46 +0000

    Update two_sum

--- Array/11_two_sum.py
+++ Array/11_two_sum.py
@@ -1,22 +1,27 @@
 """
-11. Find 2 numbers from whose sum is the targeted input number
+11. Two sum
 """
 
 arr=list(map(int,input("Enter your elements: ").split()))
 trgt=int(input("Enter the targeted no.: "))
-
+arr.sort()
 i=0
 j=len(arr)-1
 x=[]
 while i<j:
     sum=arr[i]+arr[j]
     if sum == trgt:
-        x.append((i,j))
-        i+=1
+        x.append((arr[i],arr[j]))
         j-=1
+        i+=1
+        while i<j and arr[i]==arr[i-1]:
+            i+=1
+        while i<j and arr[j]==arr[j+1]:
+            j-=1
     elif sum<trgt:
         i+=1
     else:
         j-=1
 
-print(f"INDEX{x}")
+
+print(f"{x}")
