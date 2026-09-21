be2feed9f2024cf6d0ffe132d4ccfc1b8c893f9b
Sumit-Ghosh-771J <Sumit-Ghosh-771J@users.noreply.github.com>
2026-09-21 16:23:52 +0000

    Update maximum_of_all_sub_arrays_of_size_k

--- Array/20_maximum_of_all_sub_arrays_of_size_k.py
+++ Array/20_maximum_of_all_sub_arrays_of_size_k.py
@@ -2,20 +2,21 @@
 20. Maximum of all sub-arrays of size k
 """
 
+from collections import deque
 arr=list(map(int,input("Enter the elements: ").split()))
 k=int(input("Enter the window size: "))
-q=[]
+q=deque()
 ans=[]
 for i in range(k):
     while q and arr[q[-1]]<arr[i]:
-        q.pop(-1)
+        q.pop()
     q.append(i)
 ans.append(arr[q[0]])
 for i in range(k,len(arr)):
     if q and q[0]<=i-k:
-        q.pop(0)
+        q.popleft()
     while q and arr[q[-1]]<arr[i]:
-        q.pop(-1)
+        q.pop()
     q.append(i)
     if q:
         ans.append(arr[q[0]])
