de5bb6c2c7463f6e92e172b517acb476fd34b664
Sumit-Ghosh-771J <Sumit-Ghosh-771J@users.noreply.github.com>
2026-09-22 16:15:44 +0000

    Add next_greater_element_using_monotonic_stack

---
+++ Array/21_next_greater_element_using_monotonic_stack.py
@@ -0,0 +1,19 @@
+"""
+21. Next greater element using monotonic stack
+"""
+
+arr = list(map(int, input("Enter the elements: ").split()))
+
+stack = []
+ans = [-1] * len(arr)
+for i in range(len(arr) - 1, -1, -1):
+
+
+  while stack and stack[-1] <= arr[i]:
+    stack.pop()
+  if stack:
+    ans[i] = stack[-1]
+
+  stack.append(arr[i])
+
+print(ans)
